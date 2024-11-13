2780aff43890b73161a9e2fd4d22ab69b4506625
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-11-13 10:23:03 +0600

    added moment js for time format

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
-
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <h1 className="text-7xl font-normal text-center">Dragon News</h1>
+            <h3 className="text-3xl font-bold text-center mt-1 uppercase">Journalism without Fear and Favour</h3>
+            <p className="text-2xl font-normal text-center mt-1">{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
