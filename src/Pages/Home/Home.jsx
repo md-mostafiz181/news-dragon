2400423ca2de8545e6fd38cdc918d8c429c8a54a
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-11-13 10:08:18 +0600

    show ui in the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,26 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h1 className="text-3xl font-bold"> This is Home</h1>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h1>News coming soon</h1>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h1>Right side</h1>
+        </div>
+    );
+};
+
+export default RightSideNav;
