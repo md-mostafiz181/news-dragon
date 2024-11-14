578f3b52dc207783418381d1a27b2ecd1587fa0e
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-11-14 08:26:25 +0600

    show all category news

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories,setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch("categories.json")
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    }, [])
     return (
-        <div>
-            <h1>Left side</h1>
+        <div className="space-y-5">
+            <h1 className="text-3xl font-bold">All Category :{categories.length} </h1>
+
+            <div className="my-3">
+            {
+                categories.map(category=> <Link className="block py-1"
+                     key={category.id}
+                     to={`/category/${category.id}`}
+                     ><button className="btn btn-active btn-ghost w-full">{category.name}</button></Link> )
+            }
+            </div>
         </div>
     );
 };
